54287865493eb153b2805b86028c79d1c1f4c70a
Artem Chernyshev <artem.chernyshev@red-soft.ru>
2024-01-22 14:35:09 +0300

    freerdp parser added

--- pkg_handlers.py
+++ pkg_handlers.py
@@ -1076,6 +1076,16 @@ class PkgHandler:
                 'assigned_to': int(self.users_dict['vladislav.mitin']),
                 'watchers': None,
             },
+            'FreeRDP': {
+                'check_func': self.is_freerdp_issue,
+                'cve_counter': 0,
+                'stapel_name': 'freerdp',
+                'nvr_list': [self.get_latest_rpm_data("freerdp", tag[0], tag[1]).get('version', "")
+                             for tag in self.tags],
+                'check_patch': False,
+                'assigned_to': int(self.users_dict['alexey.rodionov']),
+                'watchers': None,
+            },
         }
 
     # Нижеследующие функции проверяют, относится ли уязвимость к соответствующему пакету
@@ -2635,6 +2645,24 @@ class PkgHandler:
                     return IsXIssue.YES
 
         if cpe and (cpe[0].split(":")[4] == 'openvswitch'):
+            return IsXIssue.YES
+
+        return IsXIssue.MAYBE
+
+    @staticmethod
+    def is_freerdp_issue(desc, links, cpe) -> IsXIssue:
+        if 'freerdp' not in split_and_strip(desc):
+            return IsXIssue.NO
+
+        for link in links:
+            netloc = parse.urlparse(link).netloc
+            path_split = parse.urlparse(link).path.split('/')
+            if len(path_split) > 2:
+                if netloc == 'github.com' and \
+                        (path_split[1] == 'FreeRDP' and path_split[2] == 'FreeRDP'):
+                    return IsXIssue.YES
+
+        if cpe and (cpe[0].split(":")[4] == 'freerdp'):
             return IsXIssue.YES
 
         return IsXIssue.MAYBE
