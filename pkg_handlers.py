eb7ceefe862ade85068cfbedf9ee41f9a0534e9e
Artem Chernyshev <artem.chernyshev@red-soft.ru>
2024-01-22 15:09:30 +0300

    atril parser added

--- pkg_handlers.py
+++ pkg_handlers.py
@@ -1116,6 +1116,16 @@ class PkgHandler:
                 'assigned_to': int(self.users_dict['vladimir.chirkin']),
                 'watchers': None,
             },
+            'Atril': {
+                'check_func': self.is_atril_issue,
+                'cve_counter': 0,
+                'stapel_name': 'atril',
+                'nvr_list': [self.get_latest_rpm_data("atril", tag[0], tag[1]).get('version', "")
+                             for tag in self.tags],
+                'check_patch': False,
+                'assigned_to': int(self.users_dict['anton.savin']),
+                'watchers': None,
+            },
         }
 
     # Нижеследующие функции проверяют, относится ли уязвимость к соответствующему пакету
@@ -2756,6 +2766,24 @@ class PkgHandler:
                     return IsXIssue.YES
 
         if cpe and (cpe[0].split(":")[4] == 'kate'):
+            return IsXIssue.YES
+
+        return IsXIssue.MAYBE
+
+    @staticmethod
+    def is_atril_issue(desc, links, cpe) -> IsXIssue:
+        if 'atril' not in split_and_strip(desc):
+            return IsXIssue.NO
+
+        for link in links:
+            netloc = parse.urlparse(link).netloc
+            path_split = parse.urlparse(link).path.split('/')
+            if len(path_split) > 2:
+                if netloc == 'github.com' and \
+                        (path_split[1] == 'mate-desktop' and path_split[2] == 'atril'):
+                    return IsXIssue.YES
+
+        if cpe and (cpe[0].split(":")[4] == 'atril'):
             return IsXIssue.YES
 
         return IsXIssue.MAYBE
