d9a7527710efb6529f8b55e9f3d567af0147950e
Artem Chernyshev <artem.chernyshev@red-soft.ru>
2024-01-31 17:56:46 +0300

    rpm parser added

--- pkg_handlers.py
+++ pkg_handlers.py
@@ -1194,6 +1194,16 @@ class PkgHandler:
                 'assigned_to': int(self.users_dict['dmitry.safonov']),
                 'watchers': None,
             },
+            'RPM': {
+                'check_func': self.is_rpm_issue,
+                'cve_counter': 0,
+                'stapel_name': 'rpm',
+                'nvr_list': [self.get_latest_rpm_data("rpm", tag[0], tag[1]).get('version', "")
+                             for tag in self.tags],
+                'check_patch': False,
+                'assigned_to': int(self.users_dict['vitaly.peshcherov']),
+                'watchers': None,
+            },
         }
 
     # Нижеследующие функции проверяют, относится ли уязвимость к соответствующему пакету
@@ -2982,6 +2992,24 @@ class PkgHandler:
                 return IsXIssue.YES
 
         if cpe and (cpe[0].split(":")[3] == 'postgresql' and cpe[0].split(":")[4] == 'postgresql'):
+            return IsXIssue.YES
+
+        return IsXIssue.MAYBE
+
+    @staticmethod
+    def is_rpm_issue(desc, links, cpe) -> IsXIssue:
+        if 'rpm' not in split_and_strip(desc):
+            return IsXIssue.NO
+
+        for link in links:
+            netloc = parse.urlparse(link).netloc
+            path_split = parse.urlparse(link).path.split('/')
+            if len(path_split) > 2:
+                if netloc == 'github.com' and \
+                        (path_split[1] == 'rpm-software-management' and path_split[2] == 'rpm'):
+                    return IsXIssue.YES
+
+        if cpe and (cpe[0].split(":")[3] == 'rpm' and cpe[0].split(":")[4] == 'rpm'):
             return IsXIssue.YES
 
         return IsXIssue.MAYBE
